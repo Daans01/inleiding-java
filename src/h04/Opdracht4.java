171e6bbbfb7723b6b735fd5f9419230c48230f12
Daan van der Sman <dvandersman@roc-dev.com>
2019-04-25 14:36:05 +0200

    h01,2,4 opdrachten

---
+++ src/h04/Opdracht4.java
@@ -0,0 +1,40 @@
+package h04;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Opdracht4 extends Applet {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void paint(Graphics g) {
+
+
+
+
+        g.setColor(Color.black);
+        g.drawLine(100, 430, 100, 430);
+
+        g.setColor(Color.black);
+        g.drawLine(100, 330, 80, 330);
+
+        g.setColor(Color.black);
+        g.drawLine(100, 230, 80, 230);
+
+        g.setColor(Color.black);
+        g.drawLine(100, 130, 80,130);
+
+        g.setColor(Color.black);
+        g.drawLine(100, 30, 80, 30);
+
+        g.setColor(Color.black);
+        g.drawLine(100, 30, 100, 430);
+
+
+    }
+
+}
--- src/h04/Welkom.java
+++ src/h04/Welkom.java
@@ -11,9 +11,24 @@ public class Welkom extends Applet {
     }
     @Override
     public void paint (Graphics g) {
-        g.drawLine(150,280,150,100);
-        g.drawLine(200,280,150,100);
-        g.drawLine(50,160,150,160);
+        g.drawLine(250,150,250,300);
+        g.drawLine(75,150,75,300);
+        g.drawLine(75,150,250,150);
+        g.drawLine(75,300,250,300);
+
+        g.drawLine(75,150,160,60);
+        g.drawLine(160,60,250,150);
+
+        g.drawLine(100,180,100,220);
+        g.drawLine(140,180,140,220);
+        g.drawLine(100,180,140,180);
+        g.drawLine(100,220,140,220);
+
+        g.drawLine(150,300,150,240);
+        g.drawLine(190,300,190,240);
+        g.drawLine(150,240,190,240);
+
+
     }
 }
 
