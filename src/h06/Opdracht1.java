0e61b3b592713f2003c65e43586c29fb5b6a0bc1
Daan van der Sman <dvandersman@roc-dev.com>
2019-05-09 10:43:40 +0200

    h01,2,4,5,6

---
+++ src/h06/Opdracht1.java
@@ -0,0 +1,31 @@
+package h06;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Opdracht1 extends Applet {
+
+    int kinderen, bedrag;
+    double uitkomst;
+
+    public void init() {
+        kinderen = 4;
+        bedrag = 113;
+        uitkomst = bedrag / kinderen;
+
+    }
+
+    @Override
+    public void paint(Graphics g) {
+
+        g.drawString("De uitkomst is: " + uitkomst, 20, 20);
+        g.drawString("Jan krijgt €28 ", 20, 30);
+        g.drawString("Ali krijgt €28", 20, 40);
+        g.drawString("Jeannette krijgt €28", 20, 50);
+        g.drawString("Ik krijgt €28", 20, 60);
+    }
+
+
+
+
+    }
