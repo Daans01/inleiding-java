61f24f5ceb7b64a845538e05aab579dfd1e45399
Daan van der Sman <dvandersman@roc-dev.com>
2019-06-20 09:23:56 +0200

    h01,2,4,5,6,8

---
+++ src/h010/Opdracht1en2.java
@@ -0,0 +1,39 @@
+package h010;
+
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+
+public class Opdracht1en2 extends Applet {
+    TextField tekst1;
+    int hoogstegetal, laagstegetal;
+    int getal;
+
+    public void init() {
+        tekst1 = new TextField("",10);
+        tekst1Listener t1l = new tekst1Listener();
+        tekst1.addActionListener( t1l );
+        add (tekst1);
+    }
+
+    public void paint (Graphics g){
+        g.drawString("Hoogste getal =  " + hoogstegetal,20,60);
+        g.drawString("Laagste getal =  "+ laagstegetal,20,80);
+
+    }
+
+    class tekst1Listener implements ActionListener {
+        public void actionPerformed( ActionEvent e ) {
+            getal = Integer.parseInt(tekst1.getText());
+            if (getal > hoogstegetal) {
+                hoogstegetal = getal;
+                repaint();
+            }
+            else if (getal < hoogstegetal) {
+                laagstegetal = getal;
+                repaint();
+            }
+
+        }
+    }
+}
