240dc7975b560cff0f775d3e00dc52184f77f2f0
Daan van der Sman <dvandersman@roc-dev.com>
2019-06-21 09:40:12 +0200

    h01,2,4,5,6,8

---
+++ src/h011/Opdracht6.java
@@ -0,0 +1,31 @@
+package h011;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht6 extends Applet {
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+        int width = 10;
+        int height = 10;
+
+        int x = 190;
+        int y = 150;
+
+        for (int i = 0; i < 5; i++) {
+
+            g.drawOval(x, y, width, height);
+
+            width += 10;
+            height += 10;
+
+            x -= 5;
+            y -= 5;
+
+        }
+
+    }
+}
---
+++ src/h011/Opdracht8.java
@@ -0,0 +1,24 @@
+package h011;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht8 extends Applet {
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+        int width = 10;
+        int height = 10;
+
+        for (int i = 0; i < 100; i++) {
+            g.drawOval(30,30,width,height);
+
+            width += 10;
+            height += 10;
+        }
+
+    }
+}
